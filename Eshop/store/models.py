291de8d8445b3b248d38a786ad74a0304cd417f5
Nitin Bargaje <nitinbargaje7@gmail.com>
2020-12-21 21:35:16 +0530

    category wise product

--- Eshop/store/models.py
+++ Eshop/store/models.py
@@ -6,10 +6,26 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=20)
 
+    def __str__(self):
+        return self.name
+
+    @staticmethod
+    def get_all_categories():
+        return Category.objects.all()
+
 
 class Product(models.Model):
     name = models.CharField(max_length=20)
     price = models.IntegerField(default=0)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     image = models.ImageField(upload_to='uploads/products')
-    description = models.CharField(max_length=200)
+    description = models.CharField(
+        max_length=200, default='', null=True, blank=True)
+
+    @staticmethod
+    def get_all_product():
+        return Product.objects.all()
+
+    @staticmethod
+    def get_all_product_by_category(category_id):
+        return Product.objects.filter(category=category_id)
--- Eshop/store/views.py
+++ Eshop/store/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Product, Category
 
 # Create your views here.
+
+
+def index(request):
+    category_id = request.GET.get('category')
+    if category_id:
+        products = Product.get_all_product_by_category(category_id)
+    else:
+        products = Product.get_all_product()
+    categories = Category.get_all_categories()
+    data = {}
+    data['products'] = products
+    data['categories'] = categories
+    return render(request, 'store.html', data)
